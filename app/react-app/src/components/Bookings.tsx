3cfa15ba788bb11f87d555c3e739b34a92ba1ebc
alek98 <alek@spectra-alarm.com>
2021-07-22 16:43:04 +0200

    toggle new booking

--- app/react-app/src/components/Bookings.tsx
+++ app/react-app/src/components/Bookings.tsx
@@ -4,16 +4,18 @@ import NewBooking from "./NewBooking"
 
 const Bookings = () => {
   const [bookings, setBookings] = useState([])
-
+  const [showForm, setShowForm] = useState(false)
+  const toggleNewBooking = () => setShowForm(!showForm)
   return (
     <div>
-      {/* <button
-        style={{ fontSize: '1.3rem', textAlign: 'center', margin: 'auto', display: 'block' }}>
-        New booking
-      </button> */}
-      <NewBooking/>
-      <Days/>
-
+      {showForm ? 
+      <NewBooking onCancel={toggleNewBooking} onSave={toggleNewBooking} /> :
+        <button
+          onClick={toggleNewBooking}
+          style={{ fontSize: '1.3rem', textAlign: 'center', margin: 'auto', display: 'block' }}>
+          New booking
+        </button>}
+      <Days />
     </div>
   )
 }
--- app/react-app/src/components/NewBooking.tsx
+++ app/react-app/src/components/NewBooking.tsx
@@ -1,20 +1,22 @@
+import { FC } from "react"
 
-const NewBooking = () => {
+const NewBooking: FC<Props> = (props) => {
   const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursady', 'Friday', 'Saturday', 'Sunday']
   return (
-    <div style={{ 
+    <div style={{
       border: '1px solid #581b98',
       borderRadius: '1rem',
       padding: '1rem',
       boxShadow: '0px 8px 15px rgb(0 0 0 / 10%)',
-      background: '#581b9861'}}>
+      background: '#581b9861'
+    }}>
 
       <span style={{ display: 'inline-block' }}>
         <label htmlFor="day" style={{ display: 'block' }}>Day</label>
         <select name="day" >
           <option value=''>--Choose a day--</option>
           {days.map(day =>
-            <option value={day}>{day}</option>)}
+            <option key={day} value={day}>{day}</option>)}
         </select>
       </span>
 
@@ -29,11 +31,19 @@ const NewBooking = () => {
       </span>
 
       <div >
-        <button style={{ color: '#f3558e', backgroundColor: 'white' }}>Cancel</button>
-        <button>Save</button>
+        <button
+          style={{ color: '#f3558e', backgroundColor: 'white' }}
+          onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button onClick={props.onSave}>Save</button>
       </div>
     </div>
   )
 }
 
+interface Props {
+  onCancel: () => void,
+  onSave: () => void,
+}
 export default NewBooking
