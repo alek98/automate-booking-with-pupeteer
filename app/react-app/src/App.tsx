cc99ba0714c56c188b5680bd0ee251edb971327a
alek98 <alek@spectra-alarm.com>
2021-07-23 17:31:08 +0200

    connected with firestore

--- app/react-app/src/App.tsx
+++ app/react-app/src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css';
 import Bookings from './components/Bookings';
+import { db } from './firebase/Config';
 function App() {
+  db.doc('users/alek').set({name: 'alek', surname: 'vuj'})
   return (
     <div className="App">
       <h1 style={{color:'#9c1de7'}}>Booking</h1>
---
+++ app/react-app/src/firebase/Config.tsx
@@ -0,0 +1,22 @@
+import firebase from "firebase/app";
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAorDERlI_-xfFdUaNjUiZof9YuGQPaZwI",
+  authDomain: "automate-booking-with-pupeteer.firebaseapp.com",
+  projectId: "automate-booking-with-pupeteer",
+  storageBucket: "automate-booking-with-pupeteer.appspot.com",
+  messagingSenderId: "763712958110",
+  appId: "1:763712958110:web:84068eefbe4f0c283f958f"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+
+const db = firebaseApp.firestore();
+if(window.location.hostname === 'localhost') {
+  console.log('using localhost')
+  db.useEmulator('localhost', 8080);
+} else {
+  console.log('not using localhost')
+}
+export {db}
