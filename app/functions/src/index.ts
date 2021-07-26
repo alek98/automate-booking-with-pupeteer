0b0b02749e647b89c3abf3d707566a3b1a01599e
alek98 <alek@spectra-alarm.com>
2021-07-26 10:41:13 +0200

    created simple function with puppeteer

--- app/functions/src/index.ts
+++ app/functions/src/index.ts
@@ -1,5 +1,8 @@
 import * as functions from "firebase-functions";
 
+import * as admin from 'firebase-admin'
+admin.initializeApp()
+
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
@@ -7,3 +10,5 @@ export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
+
+export * from './puppeteer'
---
+++ app/functions/src/puppeteer.ts
@@ -0,0 +1,13 @@
+import * as functions from 'firebase-functions'
+// import * as admin from 'firebase-admin'
+import * as puppeteer from 'puppeteer'
+
+export const createBookingWithPuppeteer = functions.runWith({memory: '1GB'}).https.onRequest(async (request, response) => {
+  // let browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] })
+  let browser = await puppeteer.launch({headless: false, slowMo: 50})
+  // let browser = await puppeteer.launch({headless: true})
+  let page = await browser.newPage()
+  await page.goto('https://github.com')
+  await page.setViewport({ width: 1280, height: 1276 })
+  response.send('done')
+});
