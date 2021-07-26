35fcca6bbe55dd9bd72fea6f13e1cf289548463a
alek98 <alek@spectra-alarm.com>
2021-07-26 11:51:35 +0200

    hot fix

--- app/functions/src/puppeteer.ts
+++ app/functions/src/puppeteer.ts
@@ -1,26 +1,23 @@
 import * as functions from 'firebase-functions'
 // import * as admin from 'firebase-admin'
 import * as puppeteer from 'puppeteer'
-import {localConfig} from './config'
+import { localConfig } from './config'
 
 let config: any;
-if(functions.config()?.length) {
-  config = functions.config()
-} else {
-  config = localConfig
-}
+if (functions.config()?.length) config = functions.config()
+else config = localConfig
+let service = config.service
 
 export const createBookingWithPuppeteer = functions
   .runWith({ memory: '1GB', timeoutSeconds: 120 })
   .https.onRequest(async (request, response) => {
     // let browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] })
-    // let browser = await puppeteer.launch({ headless: false, slowMo: 50 })
-    let browser = await puppeteer.launch({headless: true})
+    let browser = await puppeteer.launch({ headless: false, slowMo: 50 })
+    // let browser = await puppeteer.launch({headless: true})
     let page = await browser.newPage()
-    await page.goto('https://github.com')
+    await page.goto(service.website)
     await page.setViewport({ width: 1280, height: 1276 })
 
-    let service = config.service
-    console.log(service)
+    console.log(service.website)
     response.send('done')
   });
